181bb636ceb83159f6402cbed040a462a772236e
rajdeeprath <rajdeeprath@gmail.com>
2017-08-12 02:29:45 +0530

    Red5 Utilities Maven project
    + Created Red5 Utilities maven project to contain all utility stuff.
    + Added custom file name generator for now : To relocate recordings

---
+++ utilities/red5-utilities/src/main/java/org/red5/server/api/utilities/App.java
@@ -0,0 +1,20 @@
+package org.red5.server.api.utilities;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+
+    
+    public static void main( String[] args )
+    {
+        
+    }
+}
---
+++ utilities/red5-utilities/src/main/java/org/red5/server/api/utilities/CustomFilenameGenerator.java
@@ -0,0 +1,58 @@
+package org.red5.server.api.utilities;
+
+
+import org.red5.server.api.scope.IScope;
+import org.red5.server.api.stream.IStreamFilenameGenerator;
+
+public class CustomFilenameGenerator implements IStreamFilenameGenerator {
+
+	/** Path that will store recorded videos. */ 
+	 public String recordPath = "streams/"; 
+	 /** Path that contains VOD streams. */ 
+	 public String playbackPath = "streams/"; 
+	 /** Set if the path is absolute or relative */ 
+	 public boolean resolvesAbsolutePath = false; 
+	 
+	 public String generateFilename(IScope scope, String name, GenerationType type) 
+	 { 
+	  return generateFilename(scope, name, null, type); 
+	 }
+	 
+	 
+	 
+	 public String generateFilename(IScope scope, String name, String extension, GenerationType type) 
+	 { 
+		  String filename; 
+		  
+		  if (type == GenerationType.RECORD) 
+		   filename = recordPath + name; 
+		  else 
+		   filename = playbackPath + name; 
+		  
+		  if (extension != null)
+		  filename += extension; 
+		  
+		  return filename; 
+	 } 
+	 
+	 public boolean resolvesToAbsolutePath() 
+	 { 
+		 return resolvesAbsolutePath; 
+	 } 
+	 
+	 public void setRecordPath(String path)
+	 { 
+		 recordPath = path; 
+	 }
+	 
+	 public void setPlaybackPath(String path) 
+	 { 
+		 playbackPath = path; 
+	 } 
+	
+	 public void setAbsolutePath(Boolean absolute)
+	 { 
+		 resolvesAbsolutePath = absolute; 
+	 }
+
+}
